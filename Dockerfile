82a2cae8669c3d1c92b57d61f1fb362aff1a0674
Henry Abravanel <henryabra@gmail.com>
2018-12-31 00:27:36 +0200

    Added PORT enviroment variable for generic implementation

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,7 @@ FROM alpine
 RUN apk update && \
       apk upgrade && \
       apk add --no-cache miniupnpc
+ENV PORT 8123
 COPY set-upnp.sh /
-CMD ["sh" , "set-upnp.sh"]
+
+CMD ["sh", "-c", "/set-upnp.sh $PORT"]
--- set-upnp.sh
+++ set-upnp.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
-trap 'upnpc -d 8123 TCP' SIGTERM
-upnpc -e "Docker upnpc HA" -a `upnpc -l | grep "Local LAN ip address" | cut -d: -f2` 8123 8123 TCP
+#!/usr/bin/env sh
+trap "upnpc -d $1 TCP" TERM
+upnpc -e "Docker upnpc HA" -a `upnpc -l | grep "Local LAN ip address" | cut -d: -f2` $1 $1 TCP
 tail -f /dev/null & wait
