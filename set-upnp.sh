620f15873d286e8befdf35d54cbcbfb4c703faae
Henry Abravanel <henryabra@gmail.com>
2018-12-31 22:58:39 +0200

    Added update loop for upnpc every 5 minutes

--- set-upnp.sh
+++ set-upnp.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 trap "upnpc -d $1 TCP" TERM
-upnpc -e "Docker upnpc HA" -a `upnpc -l | grep "Local LAN ip address" | cut -d: -f2` $1 $1 TCP
-tail -f /dev/null & wait
+
+while true
+do
+	upnpc -e "Docker upnpc HA" -r $1 TCP
+	sleep 300
+done
